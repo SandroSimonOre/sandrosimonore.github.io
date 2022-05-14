6bf31003b27e9f031aa3c46c3cc642fdc5f771ad
Sandro <sandrosimonore@gmail.com>
2022-05-14 01:23:26 -0500

    menu mobile casi terminado

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// alert('hola')
+//const menu = document.querySelector('.menu');
+const menu = document.querySelector('nav');
+const openMenuBtn = document.querySelector('.open-menu');
+const closeMenuBtn = document.querySelector('.close-menu');
+
+function toggleMenu() {
+    menu.classList.toggle('menu_opened')
+}
+
+openMenuBtn.addEventListener('click', toggleMenu);
+closeMenuBtn.addEventListener('click', toggleMenu);
