a463d9168f1f6ff63423f74e943fb338bf061cda
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-29 01:43:24 -0500

    Languages feature in progress

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
 const flagButton = document.getElementById('flag-button');
 flagButton.addEventListener('click', () => {
     document.getElementById('menu').classList.toggle('language-toggle__menu--visible');
-})
+    
+    /*setTimeout(() => {
+        document.getElementById('menu').classList.remove('language-toggle__menu--visible');
+    }, 3000)
+    */
+    
+});
 
+const menu = document.getElementById('menu');
+menu.addEventListener('click', e => {
+    const language = e.target.dataset.language;
+    
+    if (language === 'esp') {
+        flagButton.classList.add( `language-toggle__flag-button--esp`);
+    } else if (language === 'eng') {
+        flagButton.classList.remove( `language-toggle__flag-button--esp`);
+    }
+    
+    document.getElementById('menu').classList.remove('language-toggle__menu--visible');
+    loadLanguage(language);
+});
+
+const loadLanguage = async language => {
+    const texts = await (await fetch(`../languages/${language}.json`)).json();
+    const textsToChange = document.querySelectorAll("[data-section]");
+    for (const textToChange of textsToChange ) {
+        const section = textToChange.dataset.section;
+        const value = textToChange.dataset.value;
+        textToChange.innerHTML = texts[section][value];
+    }
+    //console.log(jsonLanguage)
+}
+
+/*
 const eng = document.getElementById('eng');
-const esp = document.getElementById('esp');
+eng.addEventListener('click', ()=> {
+    flagButton.classList.remove('language-toggle__flag-button--esp');
+    document.getElementById('menu').classList.remove('language-toggle__menu--visible');
+});
 
+const esp = document.getElementById('esp');
+esp.addEventListener('click', ()=> {
+    flagButton.classList.add('language-toggle__flag-button--esp');
+    document.getElementById('menu').classList.remove('language-toggle__menu--visible');
+});
+*/
