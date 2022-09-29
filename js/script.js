66a40192e53bd330a044ae19655f95c93906004e
SANDRO SIMON <sandrosimonore@gmail.com>
2022-09-28 21:00:22 -0500

    Language toggle button implemented

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const flagButton = document.getElementById('flag-button');
+flagButton.addEventListener('click', () => {
+    document.getElementById('menu').classList.toggle('language-toggle__menu--visible');
+})
+
+const eng = document.getElementById('eng');
+const esp = document.getElementById('esp');
+
